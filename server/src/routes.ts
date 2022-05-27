02ccf4c2640dc5b6c5287baf504af5348146fc89
Alexander kruger <alex.bigbox@gmail.com>
2022-05-27 08:02:29 -0300

    erro 404

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,20 +8,27 @@ export const routes = express.Router()
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  const prismaFeedbackRepository = new PrismaFeedbackRepository;
-  const nodemailerMailAdapter = new NodemailerMailAdapter()
+  try {
 
-  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbackRepository,
-    nodemailerMailAdapter
-  )
+    const prismaFeedbackRepository = new PrismaFeedbackRepository;
+    const nodemailerMailAdapter = new NodemailerMailAdapter()
 
-  await submitFeedbackUseCase.execute({
-    type,
-    comment,
-    screenshot,
-  })
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+      prismaFeedbackRepository,
+      nodemailerMailAdapter
+    )
+
+    await submitFeedbackUseCase.execute({
+      type,
+      comment,
+      screenshot,
+    })
 
 
-  return res.status(201).send();
+    return res.status(201).send();
+  } catch (err) {
+    console.error(err);
+
+    return res.status(500).send();
+  }
 })
